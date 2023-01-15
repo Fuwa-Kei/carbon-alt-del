0337f49a0d9da6678c1c7bcaf3138dc67f523d44
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-01-15 03:25:05 +0000

    Update productDetails.js

--- src/assets/products/productDetails.js
+++ src/assets/products/productDetails.js
@@ -1,69 +1,69 @@
 const productDetails = [
     {
-        id: "1",
+        id: 1,
         image: require('../../assets/products/butter.jpg'),
         name: "Butter",
         carbon: "A",
         price: 3.67
     },
     {
-        id: "2",
+        id: 2,
         image: require('../../assets/products/chocolate.jpg'),
         name: "Chocolate",
         carbon: "A",
         price: 12.45
     },
     {
-        id: "3",
+        id: 3,
         image: require('../../assets/products/eggs.jpg'),
         name: "Egg",
         carbon: "A",
         price: 2.99
     },
     {
-        id: "4",
+        id: 4,
         image: require('../../assets/products/flour.jpg'),
         name: "Flour",
         carbon: "A",
         price: 7.89
     },
     {
-        id: "5",
+        id: 5,
         image: require('../../assets/products/honey.jpg'),
         name: "Honey",
         carbon: "A",
         price: 3.49
     },
     {
-        id: "6",
+        id: 6,
         image: require('../../assets/products/meat.jpg'),
         name: "Meat",
         carbon: "A",
         price: 7.99
     },
     {
-        id: "7",
+        id: 7,
         image: require('../../assets/products/oranges.jpg'),
         name: "Oranges",
         carbon: "A",
         price: 1.68 
     },
     {
-        id: "8",
+        id: 8,
         image: require('../../assets/products/sugar.jpg'),
         name: "Sugar",
         carbon: "A",
         price: 10.76
     },
     {
-        id: "9",
+        id: 9,
         image: require('../../assets/products/butter.jpg'),
         name: "Butter",
         carbon: "A",
         price: 2.99
     },
     {
-        id: "10",
+        id: 10,
         image: require('../../assets/products/strawberries.jpg'),
         name: "Strawberries",
         carbon: "A",
