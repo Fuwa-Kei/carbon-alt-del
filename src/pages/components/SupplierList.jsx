8e512bd5ff7ec1d798787a3d3f15cb1a74b8f567
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-01-16 14:30:07 +0000

    Update SupplierList.jsx

--- src/pages/components/SupplierList.jsx
+++ src/pages/components/SupplierList.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { ShopContext } from '../../context/shop-context'
 import { useParams } from 'react-router-dom'
 import productDetails from '../../assets/products/productDetails';
@@ -11,13 +11,20 @@ function SupplierList({name, offset, price}) {
     const singleProduct = productDetails.find(product => product.id === parseInt(productId))
     const id = singleProduct.id;
 
+    const [addCart, setAdded] = useState("Add To Cart");
+
+    function handleClick() {
+      addToCart(id);
+      setAdded("Added")
+    }
+
   return (
     <div className="supplier-list">
         <div className="supplier">
             <div className="supplier-name">{name}</div>
             <div className="offset-price">Carbon Offset Price: ${offset}</div>
             <div className="product-price">Price: ${price}</div>
-            <div className="add-cart" onClick={() => addToCart(id)}>Add to Cart</div>
+            <div className="add-cart" onClick={() => handleClick()}>{addCart}</div>
         </div>
     </div>
   )
